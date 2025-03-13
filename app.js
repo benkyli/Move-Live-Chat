8156840d5467c897aa7a088410fd3aa3152ed83f
Benjamin Li <benjaminli@hotmail.ca>
2025-03-12 23:28:57 -0400

    basic implementation to retrieve dynamically loaded chat

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // Get chat box
-const chatbox = document.getElementById("chat-messages");
+
+function getchat() {
+    return document.getElementById('chat-container');
+}
+
+if (document.readyState == 'complete') {
+    console.log('loaded');
+    setTimeout(() => {
+        var chat = getchat();
+    console.log(chat);
+    },5000)
+} 
+
+
+    
+
+
+
+// Try seeing if an on click works for chat retrieval; may just not be loaded on tab opening
+// If not, perhaps it's an iframe fuckery. 
