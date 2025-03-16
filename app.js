24574c9ab02f270fdb6215109570776b365270b8
Benjamin Li <benjaminli@hotmail.ca>
2025-03-16 17:57:50 -0400

    removed redundant chat opening button

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function getElm(selector) {
+function getElem(selector) {
     return new Promise(resolve => {
         if (document.querySelector(selector)) {
             resolve(document.querySelector(selector));
@@ -19,6 +19,28 @@ function getElm(selector) {
 }
 
 // print chat
-getElm('#chat-container').then((elm) => {
-    console.log(elm);
+const chat = getElem('#chat-container');
+chat.then((response) => {
+    console.log(response);
 })
+
+// remove redundant chat opening button below the description
+const teaser = getElem('#teaser-carousel')
+teaser.then((res) => {
+    res.remove();
+})
+
+var styles = `
+    #chat-container {
+        display: flex;
+        flex-direction: column;
+    }
+`
+
+// var styleSheet = document.createElement("style")
+// styleSheet.textContent = styles
+// document.head.appendChild(styleSheet)
+
+
+
+// include some kind of cancel function if chat can't be found. Could also check if it's a video. Could also check if chat is closed. 
